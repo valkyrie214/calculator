269b23a5255ac3948b9222bf5d1c3a940cf5cfe9
valkyrie214 <valkyrie214@gmail.com>
2019-10-26 11:29:43 +0700

    perkalian

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,6 @@ public class Calculator {
     public static void main(String[] args) {
         int firstNumber = 10, secondNumber = 10;
         System.out.println(firstNumber + secondNumber);
+        System.out.println(firstNumber * secondNumber);
     }
 }
