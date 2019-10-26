1c48b23d2e88e93c574b5d22b918c01f4c9c2051
valkyrie214 <valkyrie214@gmail.com>
2019-10-26 11:27:28 +0700

    tambah

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public static void main(String[] args) {
-
+        int firstNumber = 10, secondNumber = 10;
+        System.out.println(firstNumber + secondNumber);
     }
 }
